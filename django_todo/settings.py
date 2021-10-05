a718870b03de28bbb48afa8bd6a3124590ffe5b8
TanYa-Go <tanja.godinic@gmail.com>
2021-10-05 08:33:48 +0000

    fix allowed hosts variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wri^39cq-fu2wak477u8_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['HEROKU_HOSTNAME']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
